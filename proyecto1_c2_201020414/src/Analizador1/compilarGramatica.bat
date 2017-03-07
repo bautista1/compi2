43a4ff18d943eec7b729b58c82b961eb298737a2
bautista1 <alberthbau@gmail.com>
2017-03-06 23:51:12 -0600

    hola
    mundo

--- proyecto1_c2_201020414/src/Analizador1/compilarGramatica.bat
+++
@@ -1,8 +0,0 @@
-SET JAVA_HOME="C:\javacc\javacc-6.0\bin"
-SET PATH=%JAVA_HOME%;%PATH%
-SET CLASSPATH=%JAVA_HOME%;
-cd C:\Users\Alberth\Documents\NetBeansProjects\proyecto1_c2_201020414\src\Analizador1
-javacc proyecto1.jj
-cd C:\Users\Alberth\Documents\NetBeansProjects\proyecto1_c2_201020414\src\Analizador1
-javac *.java
-pause
